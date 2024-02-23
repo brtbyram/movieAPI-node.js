8317d9fbecfeb61e7383220e19945763dfbe4420
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-02-23 19:09:43 +0300

    Update movie routes and controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -22,7 +22,66 @@ const getMovies = async (req, res) => {
   }
 };
 
+const getMovieById = async (req, res) => {
+  const { movie_id } = req.params;
+  const movie = await MovieModel.findById(movie_id);
+  try {
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const updateMovieById = async (req, res) => {
+  const { movie_id } = req.params;
+  const movie = req.body;
+  const updatedMovie = await MovieModel.findByIdAndUpdate(movie_id, movie, {
+    new: true,
+  });
+  try {
+    res.status(200).json(updatedMovie);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const deleteMovieById = async (req, res) => {
+  const { movie_id } = req.params;
+  const movie = await MovieModel.findByIdAndDelete(movie_id);
+  try {
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const getTop10Movies = async (req, res) => {
+  const movies = await MovieModel.find({}).sort({ imdb_score: -1 }).limit(10);
+  try {
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const getMoviesBetweenYears = async (req, res) => {
+  const { start_year, end_year } = req.params;
+  const movies = await MovieModel.find({
+    year: { $gte: parseInt(start_year), $lte: parseInt(end_year) }, // $gte: büyük veya eşit, $lte: küçük veya eşit
+  });
+  try {
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
 module.exports = {
   postMovie,
   getMovies,
+  getMovieById,
+  updateMovieById,
+  deleteMovieById,
+  getTop10Movies,
+  getMoviesBetweenYears,
 };
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,10 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { postMovie, getMovies } = require('../controllers/movieController');
+const {
+  postMovie,
+  getMovies,
+  getMovieById,
+  updateMovieById,
+  deleteMovieById,
+  getTop10Movies,
+  getMoviesBetweenYears,
+} = require("../controllers/movieController");
 
 /* GET users listing. */
-router.get('/', getMovies);
-
-router.post('/', postMovie)
+router.get("/", getMovies);
+router.post("/", postMovie);
+router.get("/top10", getTop10Movies);
+router.get("/between/:start_year/:end_year", getMoviesBetweenYears);
+router.get("/:movie_id", getMovieById);
+router.put("/:movie_id", updateMovieById);
+router.delete("/:movie_id", deleteMovieById);
 
 module.exports = router;
