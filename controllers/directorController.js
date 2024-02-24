195d238a00911d4239c8f9e9606941b8ad0a4320
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-02-24 14:49:37 +0300

    Add aggregation pipeline to get directors with their movies

--- controllers/directorController.js
+++ controllers/directorController.js
@@ -2,12 +2,104 @@ const mongoose = require("mongoose");
 const DirectorModel = require("../models/directorModel");
 
 const getDirectors = async (req, res) => {
-  const directors = await DirectorModel.find({});
+  const directors = await DirectorModel.aggregate([
+    // burada aggregate metodu ile director collection'ı ile movies collection'ı birleştiriliyor
+    {
+      $lookup: {
+        from: "movies", // movies collection'ı ile birleştirilecek
+        localField: "_id", // director collection'ındaki _id ile birleştirilecek
+        foreignField: "director_id", // movies collection'ındaki director_id ile birleştirilecek
+        as: "movies", // movies collection'ı movies array'ine atanıyor
+      },
+    }, // burada movies collection'ı ile director collection'ı birleştiriliyor ve movies collection'ı movies array'ine atanıyor
+    {
+      $unwind: {
+        path: "$movies", // eğer director'ın birden fazla movies'i varsa her bir movie için director bilgisi tekrarlanır
+        preserveNullAndEmptyArrays: true, // eğer director'ın hiç movies'i yoksa onu da getirir
+      },
+    }, // burada movies array'i unwind ediliyor ve her bir movie için director bilgisi tekrarlanıyor
+    {
+      $group: {
+        // burada director bilgisi tekrarlanmaması için group ediliyor
+        _id: {
+          _id: "$_id",
+          name: "$name",
+          surname: "$surname",
+          bio: "$bio",
+        },
+        movies: {
+          $push: "$movies",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: "$_id._id",
+        name: "$_id.name",
+        surname: "$_id.surname",
+        bio: "$_id.bio",
+        movies: "$movies",
+      },
+    }, // burada director bilgisi tekrarlanmaması için project ediliyor
+  ]);
+
   try {
     res.status(200).json(directors);
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
+};
+
+const getDirectorById = async (req, res) => {
+  const director = await DirectorModel.aggregate([
+    {
+      $match: {
+        _id: mongoose.Types.ObjectId(req.params.id),
+      },
+    },
+    {
+      $lookup: {
+        from: "movies",
+        localField: "_id",
+        foreignField: "director_id",
+        as: "movies",
+      },
+    },
+    {
+      $unwind: {
+        path: "$movies",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $group: {
+        _id: {
+          _id: "$_id",
+          name: "$name",
+          surname: "$surname",
+          bio: "$bio",
+        },
+        movies: {
+          $push: "$movies",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: "$_id._id",
+        name: "$_id.name",
+        surname: "$_id.surname",
+        bio: "$_id.bio",
+        movies: "$movies",
+      },
+    },
+  ]);
+
+    try {
+        res.status(200).json(director);
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
 };
 
 const postDirectors = async (req, res) => {
@@ -24,4 +116,5 @@ const postDirectors = async (req, res) => {
 module.exports = {
   getDirectors,
   postDirectors,
+  getDirectorById
 };
--- routes/directorRoutes.js
+++ routes/directorRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getDirectors, postDirectors } = require("../controllers/directorController");
+const {
+  getDirectors,
+  postDirectors,
+  getDirectorById,
+} = require("../controllers/directorController");
 
 router.get("/", getDirectors);
 router.post("/", postDirectors);
-
+router.get("/:director_id", getDirectorById);
 
 module.exports = router;
