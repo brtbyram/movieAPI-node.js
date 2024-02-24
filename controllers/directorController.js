ee1c03553112426785f7f252ec22e111b5ad6a25
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-02-24 15:12:45 +0300

    Add CRUD operations for updating and deleting directors

--- controllers/directorController.js
+++ controllers/directorController.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const express = require("express");
 const DirectorModel = require("../models/directorModel");
 
 const getDirectors = async (req, res) => {
@@ -54,25 +55,26 @@ const getDirectorById = async (req, res) => {
   const director = await DirectorModel.aggregate([
     {
       $match: {
-        _id: mongoose.Types.ObjectId(req.params.id),
+        _id: new mongoose.Types.ObjectId(req.params.director_id),
       },
     },
     {
       $lookup: {
-        from: "movies",
-        localField: "_id",
-        foreignField: "director_id",
-        as: "movies",
+        from: "movies", // movies collection'ı ile birleştirilecek
+        localField: "_id", // director collection'ındaki _id ile birleştirilecek
+        foreignField: "director_id", // movies collection'ındaki director_id ile birleştirilecek
+        as: "movies", // movies collection'ı movies array'ine atanıyor
       },
-    },
+    }, // burada movies collection'ı ile director collection'ı birleştiriliyor ve movies collection'ı movies array'ine atanıyor
     {
       $unwind: {
-        path: "$movies",
-        preserveNullAndEmptyArrays: true,
+        path: "$movies", // eğer director'ın birden fazla movies'i varsa her bir movie için director bilgisi tekrarlanır
+        preserveNullAndEmptyArrays: true, // eğer director'ın hiç movies'i yoksa onu da getirir
       },
-    },
+    }, // burada movies array'i unwind ediliyor ve her bir movie için director bilgisi tekrarlanıyor
     {
       $group: {
+        // burada director bilgisi tekrarlanmaması için group ediliyor
         _id: {
           _id: "$_id",
           name: "$name",
@@ -92,14 +94,14 @@ const getDirectorById = async (req, res) => {
         bio: "$_id.bio",
         movies: "$movies",
       },
-    },
+    }, // burada director bilgisi tekrarlanmaması için project ediliyor
   ]);
 
-    try {
-        res.status(200).json(director);
-    } catch (error) {
-        res.status(404).json({ error: error.message });
-    }
+  try {
+    res.status(200).json(director);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
 };
 
 const postDirectors = async (req, res) => {
@@ -113,8 +115,37 @@ const postDirectors = async (req, res) => {
   }
 };
 
+const updateDirectorById = async (req, res) => {
+  const { director_id } = req.params;
+  const director = req.body;
+  const updatedDirector = await DirectorModel.findByIdAndUpdate(
+    director_id,
+    director,
+    {
+      new: true,
+    }
+  );
+  try {
+    res.status(200).json(updatedDirector);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const deleteDirectorById = async (req, res) => {
+  const { director_id } = req.params;
+  const director = await DirectorModel.findByIdAndDelete(director_id);
+  try {
+    res.status(200).json(director);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getDirectors,
   postDirectors,
-  getDirectorById
+  getDirectorById,
+  updateDirectorById,
+  deleteDirectorById,
 };
--- routes/directorRoutes.js
+++ routes/directorRoutes.js
@@ -4,10 +4,14 @@ const {
   getDirectors,
   postDirectors,
   getDirectorById,
+  updateDirectorById,
+  deleteDirectorById,
 } = require("../controllers/directorController");
 
 router.get("/", getDirectors);
 router.post("/", postDirectors);
 router.get("/:director_id", getDirectorById);
+router.put("/:director_id", updateDirectorById);
+router.delete("/:director_id", deleteDirectorById);
 
 module.exports = router;
